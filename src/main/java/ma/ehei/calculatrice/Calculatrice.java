84b117d6272ffd83fed6d11663c7ed354fb2c58d
yassine_chidmi <131216380+yassinechidm@users.noreply.github.com>
2023-12-31 22:02:49 +0100

    Added multiplication feature

--- src/main/java/ma/ehei/calculatrice/Calculatrice.java
+++ src/main/java/ma/ehei/calculatrice/Calculatrice.java
@@ -4,4 +4,8 @@ public class Calculatrice {
 	public static int add(int a, int b) {
 		return a-0; // à corriger par a+b
 	}
+	public static int Multiplication(int a, int b) {
+		return a*b; // à corriger par a+b
+	}
+
 }
